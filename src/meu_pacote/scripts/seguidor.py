403fccd603076c993621469e1b1dd415bd597e2f
matheusfy <matheusy@alunos.utfpr.edu.br>
2021-03-29 17:52:35 -0300

    Desafio 1 - comentado e alerta inserido

--- src/meu_pacote/scripts/seguidor.py
+++ src/meu_pacote/scripts/seguidor.py
@@ -11,17 +11,24 @@ import time
 
 class SeguidorTurtle:
     def __init__(self):
+        # cria topico
         rospy.init_node("no_seguidor", anonymous=False)
+        
+        # se inscreve no topico de odometria do waffle 1
         rospy.Subscriber("/tb3_0/odom", Odometry, self.update_pose0)
+        
+        # se inscreve no topico de odometria do waffle 1
         rospy.Subscriber("/tb3_1/odom", Odometry, self.update_pose1)
         #self.vel_pub0 = rospy.Publisher('/tb3_0/cmd_vel', Twist, queue_size=10)
-        self.vel_pub1 = rospy.Publisher('/tb3_1/cmd_vel', Twist, queue_size=10)
-        self.pose0 = Pose()
-        self.pose1 = Pose()
+        
+        self.vel_pub1 = rospy.Publisher('/tb3_1/cmd_vel', Twist, queue_size=10) # objeto para publicar no topico de velocidade do seguidor
+        self.pose0 = Pose() # posicao do mestre
+        self.pose1 = Pose() # posicao do seguidor
         self.rate = rospy.Rate(10)
         self.max_vel = 0.22
         self.max_ang = 2.84
 
+    #funcao para pegar posicao do mestre
     def update_pose0(self, msg):
         orientation_q = msg.pose.pose.orientation
         orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
@@ -30,6 +37,7 @@ class SeguidorTurtle:
         self.pose0.y = msg.pose.pose.position.y
         self.pose0.theta = yaw
     
+    #funcao para pegar posição do seguidor
     def update_pose1(self, msg):
         orientation_q = msg.pose.pose.orientation
         orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
@@ -38,10 +46,11 @@ class SeguidorTurtle:
         self.pose1.y = msg.pose.pose.position.y
         self.pose1.theta = yaw
 
-
+    # retorna a distância entre mestre e seguidor
     def ref_distance(self):
         return np.sqrt(  (self.pose0.x - self.pose1.x)**2 + (self.pose0.y - self.pose1.y)**2)
 
+    # calcula a velocidade linear que ira enviar para o seguidor
     def linear_vel_control(self, kp = 1.5):
         distance = self.ref_distance()
         control = kp* distance
@@ -49,6 +58,7 @@ class SeguidorTurtle:
             control = self.max_vel*np.sign(control)
         return control
 
+    # calcula a velocidade angula que ira enviar para o seguidor (rotaciona)
     def angular_vel_control(self, kp=6):
         angle_r = np.arctan2(self.pose0.y - self.pose1.y,  self.pose0.x - self.pose1.x )        
         control = kp*(angle_r - self.pose1.theta)
@@ -56,10 +66,11 @@ class SeguidorTurtle:
             control = self.max_ang*np.sign(control)
         return control
 
+    #funcao que atualiza o seguidor para seguir ou parar
     def move2ref(self):
 
-        ref_tol = 0.5
-        vel_msg = Twist()
+        ref_tol = 0.5 # tolerancia de distancia entre mestre e seguidor
+        vel_msg = Twist() #objeto que publica mensagem que atualiza a velocidade 
         flag = False
  
         #while self.ref_distance() >= ref_tol:
@@ -77,8 +88,10 @@ class SeguidorTurtle:
 
                 self.rate.sleep()
                 flag = False
-            else:   
+            else:
+                # Quando a distancia entre mestre e seguidor é menor que a tolerância -> para o seguidor   
                 if flag == False:
+                    rospy.loginfo("Perto demais do mestre, desativando o motor do seguidor")
                     vel_msg.linear.x = 0
                     vel_msg.angular.z= 0
                     self.vel_pub1.publish(vel_msg)
